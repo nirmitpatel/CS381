2d22c7b023073b46c1f630f35fd5e50a4e31ac3d
nirmitpatel <nirmit@nevada.unr.edu>
2017-02-27 10:20:20 -0800

    Initial Commit

---
+++ OgreTutorial6/inc/TutorialApplication.h
@@ -0,0 +1,41 @@
+#ifndef TUTORIALAPPLICATION_H
+#define TUTORIALAPPLICATION_H
+
+#include <OgreRoot.h>
+#include <OgreWindowEventUtilities.h>
+
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+class TutorialApplication
+  : public Ogre::WindowEventListener,
+    public Ogre::FrameListener
+{
+public:
+  TutorialApplication();
+  virtual ~TutorialApplication();
+
+  bool go();
+
+private:
+  virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+
+  virtual void windowResized(Ogre::RenderWindow* rw);
+  virtual void windowClosed(Ogre::RenderWindow* rw);
+
+  Ogre::Root* mRoot;
+  Ogre::String mResourcesCfg;
+  Ogre::String mPluginsCfg;
+  Ogre::RenderWindow* mWindow;
+  Ogre::SceneManager* mSceneMgr;
+  Ogre::Camera* mCamera;
+
+  OIS::InputManager* mInputMgr;
+  OIS::Keyboard* mKeyboard;
+  OIS::Mouse* mMouse;
+
+};
+
+#endif
